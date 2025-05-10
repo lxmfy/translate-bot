528e81b0737c5ed74504cd96e7a5a0d93a3c026f
Ivan <Sudo-Ivan@users.noreply.github.com>
2025-05-09 20:48:05 -0500

    0.1.0

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="translate-bot",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "lxmfy",
+        "argostranslate",
+    ],
+    entry_points={
+        'console_scripts': [
+            'translate-bot=translate_bot.bot:main',
+        ],
+    },
+    author="LXMFy",
+    author_email="team@quad4.io",
+    description="A translation bot using LXMFy and Argos Translate",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LXMFy/translate-bot",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+) 
---
+++ translate_bot/__init__.py
@@ -0,0 +1,5 @@
+"""
+Translate Bot - A translation bot using LXMFy and Argos Translate.
+"""
+
+__version__ = "0.1.0" 
